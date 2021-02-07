67433ea3e16569f41c3b4ca5f623dd78b2a93540
Prateek <rprateek254@gmail.com>
2021-02-07 19:36:14 +0530

    added service-worker and serviceWorkerRegistration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
-import './styles/main.scss'
+import './styles/main.scss';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />,document.getElementById('root'));
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
