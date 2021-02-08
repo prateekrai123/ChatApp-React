38b7896fed2861b6ed14431090fd8f16cac5ef7c
Prateek <rprateek254@gmail.com>
2021-02-08 21:02:42 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
---
+++ src/pages/SignIn.js
@@ -0,0 +1,8 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const SignIn = () => {
+  return <div>Sign In</div>;
+};
+
+export default SignIn;
