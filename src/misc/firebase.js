eafb0dc74c6a602e212a6336069b501a2898995d
Prateek <rprateek254@gmail.com>
2021-02-09 09:32:31 +0530

    added sign in page and sign in with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,13 +1,18 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const config =  {
-    apiKey: "AIzaSyDVBCE5SF5K6mNrA3XATR6QoHTckX-tGtU",
-    authDomain: "chat-web-app-f0164.firebaseapp.com",
-    databaseURL: "https://chat-web-app-f0164-default-rtdb.firebaseio.com",
-    projectId: "chat-web-app-f0164",
-    storageBucket: "chat-web-app-f0164.appspot.com",
-    messagingSenderId: "781766453291",
-    appId: "1:781766453291:web:3c5b1f6cc008a63429626e"
-  };
+const config = {
+  apiKey: 'AIzaSyDVBCE5SF5K6mNrA3XATR6QoHTckX-tGtU',
+  authDomain: 'chat-web-app-f0164.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-f0164-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-f0164',
+  storageBucket: 'chat-web-app-f0164.appspot.com',
+  messagingSenderId: '781766453291',
+  appId: '1:781766453291:web:3c5b1f6cc008a63429626e',
+};
 
-  const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
