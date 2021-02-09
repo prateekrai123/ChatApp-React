0168ac00b6c11cc0585f07082a01c24c24e6ede3
Prateek <rprateek254@gmail.com>
2021-02-09 18:44:10 +0530

    managed user with profile comtext

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,17 +1,55 @@
-/* eslint-disable arrow-body-style */
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { auth, database } from '../misc/firebase';
 
 const ProfileContext = createContext();
 
-export const ProfileProvider = ({ childern }) => {
-  const [profile] = useState(false);
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+
+    const authUnsub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+
+          setProfile(data);
+          setIsLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+
+        setProfile(null);
+        setIsLoading(false);
+      }
+    });
+
+    return () => {
+      authUnsub();
+
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
+
   return (
-    <ProfileContext.Provider value={profile}>
-      {childern}
+    <ProfileContext.Provider value={{ isLoading, profile }}>
+      {children}
     </ProfileContext.Provider>
   );
 };
 
-export const useProfile = () => {
-  useContext(ProfileContext);
-};
+export const useProfile = () => useContext(ProfileContext);
