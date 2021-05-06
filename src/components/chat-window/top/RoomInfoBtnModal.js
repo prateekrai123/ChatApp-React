b7ed7cee37feba20c33251f422298dd8b36e1021
Prateek <rprateek254@gmail.com>
2021-05-07 00:01:29 +0530

    updated title and title image and added join chat button and functionality

--- src/components/chat-window/top/RoomInfoBtnModal.js
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -1,7 +1,9 @@
 /* eslint-disable arrow-body-style */
 import React, { memo } from 'react';
+import { useParams } from 'react-router';
 import { Button, Modal } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useRooms } from '../../../context/rooms.context';
 import { useModalState } from '../../../misc/custom-hooks';
 
 const RoomInfoBtnModal = () => {
@@ -10,6 +12,19 @@ const RoomInfoBtnModal = () => {
   const description = useCurrentRoom(v => v.description);
 
   const name = useCurrentRoom(v => v.name);
+
+  let id = 0;
+
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  rooms.map(v => {
+    if (v.id === chatId) {
+      id = v.uid;
+    }
+    return 0;
+  });
 
   return (
     <>
@@ -23,6 +38,8 @@ const RoomInfoBtnModal = () => {
         <Modal.Body>
           <h6 className="mb-1">Description</h6>
           <p>{description}</p>
+          <h6 className="mb-1">ID</h6>
+          <p>{id}</p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={close}>Close</Button>
